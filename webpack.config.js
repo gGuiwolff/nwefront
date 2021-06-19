629f9126c1f95e4742a2593a75c170e617930c5a
gGuiwolff <74604286+gGuiwolff@users.noreply.github.com>
2021-06-19 00:03:41 -0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = () => ({
+    entry: [
+        "@babel/polyfill",
+        path.join(__dirname, "src", "style.css"),
+        path.join(__dirname, "src", "index.js"),
+    ],
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js",
+    },
+    performance: {
+        hints: false,
+    },
+    devServer: {
+        hot: true,
+        contentBase: path.join(__dirname, "public"),
+        publicPath: '/',
+        proxy: {
+            "/": {
+                target: "https://vegiw.herokuapp.com",
+                secure: false,
+                changeOrigin: true,
+            },
+            "/socket.io": {
+                target: "https://vegiw.herokuapp.com",
+                secure: false,
+            },
+        },
+        port: "3000",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            url: false,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "bundle.css",
+        }),
+    ],
+});
