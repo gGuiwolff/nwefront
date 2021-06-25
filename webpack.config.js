10b6e71e7e8d3eeb9b9b0793107de211e5167970
gGuiwolff <74604286+gGuiwolff@users.noreply.github.com>
2021-06-25 03:32:56 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = () => ({
     entry: [
         "@babel/polyfill",
